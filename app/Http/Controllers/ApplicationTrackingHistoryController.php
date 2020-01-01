090530e97d7c7401b46db2eedab048b7f5db677b
aliffiansyah <mochammadaliffiansyah@gmail.com>
2020-01-02 00:29:27 +0700

    Controllers

--- app/Http/Controllers/ApplicationTrackingHistoryController.php
+++ app/Http/Controllers/ApplicationTrackingHistoryController.php
@@ -12,60 +12,7 @@ use Validator;
 
 class ApplicationTrackingHistoryController extends Controller
 {
-  // public function index()
-  // {
-  //     return response()->json(['success'=>'true','data'=>ApplicationTrackingHistory::all()],200);
-  // }
-  //
-  // public function store(Request $request)
-  // {
-  //     $validator = Validator::make($request->all(), [
-  //         'id_tracking_history' => 'required',
-  //         'id_application' => 'required',
-  //         'start_time' => 'required',
-  //         'end_time' => 'required',
-  //         'duration' => 'required',
-  //     ]);
-  //
-  //     if ($validator->fails()) {
-  //         return response()->json(['error'=>$validator->errors()], 401);
-  //     }
-  //
-  //     $applicationTrackingHistory = ApplicationTrackingHistory::create($request->all());
-  //
-  //     return response()->json(['success'=>'true','data'=>$applicationTrackingHistory],201);
-  // }
-  //
-  // public function show(ApplicationTrackingHistory $applicationTrackingHistory)
-  // {
-  //     return response()->json(['success'=>'true','data'=>$applicationTrackingHistory],200);
-  // }
-  //
-  // public function update(Request $request, ApplicationTrackingHistory $applicationTrackingHistory)
-  // {
-  //     $validator = Validator::make($request->all(), [
-  //         'id_tracking_history' => 'required',
-  //         'id_application' => 'required',
-  //         'start_time' => 'required',
-  //         'end_time' => 'required',
-  //         'duration' => 'required',
-  //     ]);
-  //
-  //     if ($validator->fails()) {
-  //         return response()->json(['error'=>$validator->errors()], 200);
-  //     }
-  //
-  //     $applicationTrackingHistory->update($request->all());
-  //
-  //     return response()->json(['success'=>'true','data'=>$applicationTrackingHistory],200);
-  // }
-  //
-  // public function delete(ApplicationTrackingHistory $applicationTrackingHistory)
-  // {
-  //     $applicationTrackingHistory->delete();
-  //
-  //     return response()->json(['success'=>'true','message'=>'successfully delete'],200);
-  // }
+
   public function acceptRespones(Request $request)
   {
         $dt = $request->data;
